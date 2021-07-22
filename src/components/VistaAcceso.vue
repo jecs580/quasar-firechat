75fa55a6b9db2a33f79732a26995897d52487240
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-07-21 20:27:12 -0400

    Diseño de vista de login/registro

---
+++ src/components/VistaAcceso.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="q-px-x1">
+        <h5>Formulario de {{acceder? 'Login': 'Registro'}}</h5>
+        <q-form class="q-gutter-md" @submit.prevent
+        ="procesarFormulario">
+            <q-input
+                label="Email"
+                v-model="email"
+            />
+            <q-input
+                label="Password"
+                v-model="password"
+            />
+            <q-btn
+                :label="acceder? 'login':'Registro'"
+                type="submit"
+                color="primary"
+            />
+            <q-btn color="primary" outline v-if="!acceder" @click="acceder = true">¿Ya tienes una cuenta?</q-btn>
+            <q-btn color="negative" outline v-else @click="acceder = false">¿No tienes una cuenta?</q-btn>
+        </q-form>
+    </div>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        const email =ref('')
+        const password =ref('')
+        const acceder =  ref(true)
+        const procesarFormulario = async()=>{
+            if (!email.value.trim()|| !password.value.trim()) {
+                console.log('Campos vacios');
+                return;
+            }
+            try {
+                
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        return {
+            email,
+            password,
+            procesarFormulario,
+            acceder
+        }
+    }
+}
+</script>
